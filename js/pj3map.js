6a00603c5356f9aec48ff9e8314662212ee8a8e3
JalenHuff <35548986+UriHuffer14@users.noreply.github.com>
2018-04-19 00:11:40 -0500

    change content string data, and run on load listener

--- js/pj3map.js
+++ js/pj3map.js
@@ -1,30 +1,30 @@
 function initMap() {
 	alert('yo yo yo');
   var here = new google.maps.Map(document.getElementbyId('canvas'), {
-  center: {lat: 41.901987, lng: -87.622139}, /*can center on variable mylocation w/ new google.maps.LatLng(, );*/
+  center: {lat: 41.901987, lng: -87.622139},
   zoom: 8
   });
 
 	
-var marker = new google.maps.Marker({
-  position: center,
-  map: here,
-	animation: goole.maps.Animation.DROP,
-	draggable:true,
-	icon: 'marker.jpg'
-	title: 'Look here!'
-});
-marker.addListener('click', toggleBounce);
+  var marker = new google.maps.Marker({
+    position: center,
+    map: here,
+	  animation: goole.maps.Animation.DROP,
+	  draggable:true,
+	  icon: 'marker.jpg'
+	  title: 'Look here!'
+  });
+  marker.addListener('click', toggleBounce);
 
-var contentString = 'Getaway Spot';
+ var contentString = 'My Summer Getaway Spot';
 
-var inforwindow = new google.maps.InfoWindow({
-content: contentString
-});
+  var inforwindow = new google.maps.InfoWindow({
+    content: contentString
+  });
 
-google.maps.event.addListener(marker, 'mouseover', function() {
-	infowindow.open(here, marker);
-});
-
+  google.maps.event.addListener(marker, 'mouseover', function() {
+	  infowindow.open(here, marker);
+  });
 
 }
+google.maps.event.addDOMListener(window, 'load', initMap);
