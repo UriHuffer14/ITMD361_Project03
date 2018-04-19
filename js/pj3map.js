7a52c79510a2330a9da1f5dc00087163d695b992
JalenHuff <35548986+UriHuffer14@users.noreply.github.com>
2018-04-19 01:56:46 -0500

    var center, zoom 15, google spelling, mouseover shortened

--- js/pj3map.js
+++ js/pj3map.js
@@ -1,19 +1,20 @@
+var center;
 function initMap() {
 	//alert('yo yo yo');
   var here = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 41.901987, lng: -87.622139},
-  zoom: 8
+  zoom: 15
   });
 
   var marker = new google.maps.Marker({
     position: center,
     map: here,
-	  animation: goole.maps.Animation.DROP,
+	  animation: google.maps.Animation.DROP,
 	  draggable:true,
-	  icon: 'media/marker.jpg',
+	  icon: 'marker.jpg',
 	  title: 'Look here!'
   });
-  marker.addListener('click', toggleBounce);
+  /*marker.addListener('click', toggleBounce);
 
   var contentString = '<h1>My Summer Getaway Spot</h1>';
 
@@ -21,16 +22,16 @@ function initMap() {
     content: contentString
   });
 
-  google.maps.event.addListener(marker, 'mouseover', function() {
+  marker.addListener('mouseover', function() {
 	  infowindow.open(here, marker);
-  });
+  });*/
 	}
-function toggleBounce() {
+/*function toggleBounce() {
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
   } else {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
 	} 
-
+*/
 //google.maps.event.addDOMListener(window, 'load', initMap);
