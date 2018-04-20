36909891ca856f494491fc8f64fa94f18ed8c6f4
JalenHuff <35548986+UriHuffer14@users.noreply.github.com>
2018-04-19 23:20:25 -0500

    market variable definition outside function

--- js/pj3map.js
+++ js/pj3map.js
@@ -1,12 +1,5 @@
 //var location = {lat: 41.901987, lng: -87.622139};
-function initMap() {
-	//alert('yo yo yo');
-  var here = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 41.901987, lng: -87.622139},
-  zoom: 16
-  });
-
-  var marker = new google.maps.Marker({
+var marker = new google.maps.Marker({
     position: {lat: 41.901987, lng: -87.622139},
     map: here,
 	  animation: google.maps.Animation.DROP,
@@ -14,6 +7,21 @@ function initMap() {
 	  //icon: '../media/marker1.png',
 	  //title: 'Look here!'
   });
+function initMap() {
+	//alert('yo yo yo');
+  var here = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 41.901987, lng: -87.622139},
+  zoom: 16
+  });
+
+  /*var marker = new google.maps.Marker({
+    position: {lat: 41.901987, lng: -87.622139},
+    map: here,
+	  animation: google.maps.Animation.DROP,
+	  draggable:true,
+	  //icon: '../media/marker1.png',
+	  //title: 'Look here!'
+  });*/
   marker.addListener('click', toggleBounce);
 
   var contentString = '<h1>My Summer Getaway Spot</h1>';
